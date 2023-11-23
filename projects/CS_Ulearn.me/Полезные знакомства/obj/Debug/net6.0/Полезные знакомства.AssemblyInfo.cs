a8e9b8c99f65fdd96431a4d5a1615a2eda84e167
Kayzel <kapitonovroman5@gmail.com>
2023-11-23 10:43:44 +0300

    backup

--- projects/CS_Ulearn.me/Полезные знакомства/obj/Debug/net6.0/Полезные знакомства.AssemblyInfo.cs
+++ projects/CS_Ulearn.me/Полезные знакомства/obj/Debug/net6.0/Полезные знакомства.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Полезные знакомства")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+255de26aafcecde14900b19135ef16a8a484f699")]
 [assembly: System.Reflection.AssemblyProductAttribute("Полезные знакомства")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Полезные знакомства")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
